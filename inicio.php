438aa1225699ccd0cf1e1c0ad5d5d548062de97b
Leo Ramirez <Leo Ramirez>
2016-10-09 23:08:39 -0700

    Cambie el estilo de las input text en el popup y el orden de las mismas

--- inicio.php
+++ inicio.php
@@ -120,7 +120,7 @@
 
 			$('.usuario').on('click', function(){
 				
-				var html = '<form action = "UsuarioBaseDeDatos.php" method = "post">Nombre: <input type = "text" name = "Nombre"><br><br>Numero Telefonico: <input type = "text" name = "Numero" placeholder="Opcional"><br><br>Su edad: <input type = "text" name = "Edad"><br><br>Correo: <input type = "text" name = "Correo"><br><br>				Fecha de Nacimiento: <input type="date" name="bday" max="2016-12-31"><br><br><button type = "submit" class = "btnGeneral">Registrarse</button></form>';
+				var html = '<form action = "UsuarioBaseDeDatos.php" method = "post">Nombre: <input class = "inputTextPopup" type = "text" name = "Nombre"><br><br>Correo: <input class = "inputTextPopup" type = "text" name = "Correo"><br><br>Numero Telefonico: <input class = "inputTextPopup" type = "number" name = "Numero" placeholder="Opcional"><br><br>Fecha de Nacimiento: <input type="date" name="bday" max="2016-12-31"><br><br><button type = "submit" class = "btnGeneral">Registrarse</button></form>';
 				$('#divEscoge').fadeOut(500).delay(1).queue(function(n){ $('#divEscoge').html(html); n();}).fadeIn(500);
 				//$('#divRegistro').html(html);
 
