15771fd5bc55e814002d77dd1598db940d290959
Victor Hernandez <vicoloco@live.com.mx>
2016-10-17 05:24:59 +0100

    Avance en el perfil

--- perfil.php
+++ perfil.php
@@ -1,16 +1,60 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title></title>
-	<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-	<link rel="stylesheet" type="text/css" href="css/animate.css">
-	<link rel="stylesheet" type="text/css" href="css/perfil.css">
-	<link rel="stylesheet" type="text/css" href="css/font-awesome-4.6.3/css/font-awesome.css">
-</head>
-<body>
-	<script src = "js/jquery-3.1.0.js"></script>
-	<script src = "js/geo/jquery.simpleWeather.min.js"></script>
-	<div class = "barraMenu"><img src="logos/LogoLlave.png" class = "imgLogo"></div>
+	<head>
+		<title></title>
+		<link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+		<link rel="stylesheet" type="text/css" href="css/animate.css">
+		<link rel="stylesheet" type="text/css" href="css/perfil.css">
+		<link rel="stylesheet" type="text/css" href="css/generalTynod.css">
+		<link rel="stylesheet" type="text/css" href="css/font-awesome-4.6.3/css/font-awesome.css">
+	</head>
+	<body>
+		<script src = "js/jquery-3.1.0.js"></script>
+		<script src = "js/geo/jquery.simpleWeather.min.js"></script>
+		<div class = "barraMenu">
+			<div class = "centro">
+				<h4 class = "ftTynod"><img src="logos/LogoLlave.png" class = "imgLogo">TyNod</h4>
+				<div class = "divBuscar">
+					<input type = "text" class = "txtBuscar" > 
+					<a class = "rmLink buscar" href = "#"><i class = "fa fa-search"></i></a>
+				</div>
+			</div>
+		</div>
 
-</body>
-</html>>
+		<script>
+			$(document).ready(function(){
+				checaLocalizacion();
+			});
+
+			function cargaLocalizacion(location, woeid) {
+				$.simpleWeather({
+					location: location,
+					woeid: woeid,
+					unit: 'c',
+					success: function(weather) {
+						var cadena = "Buscar profesionistas en " + weather.city + ", " + weather.region + ", " + weather.country;
+						$('.txtBuscar').attr('placeholder', cadena);
+						
+					},
+					error: function(error) {
+						$("#weather-info").html('<p>'+error+'</p>');
+					}
+				});
+			}
+
+			function checaLocalizacion()
+			{
+				if(navigator.geolocation)
+				{
+					navigator.geolocation.getCurrentPosition(function(position) {
+						cargaLocalizacion(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+					});
+				}
+				else
+				{
+					$('#weather-info').html('Sorry, geolocation is not supported by your web browser :-(');
+				}
+			}
+		</script>
+	</body>
+</html>
