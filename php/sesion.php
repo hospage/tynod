f93ca8c3fadef6e80bd2e5dfd076859f9a0107e9
Leo Ramirez <Leo Ramirez>
2016-10-17 20:49:19 -0700

    Compara por PHP el ingreso de un usuario

--- php/sesion.php
+++ php/sesion.php
@@ -1,8 +1,34 @@
 <?php
 
-     $nombre = $_POST['correo'];
-	 $yes = $_POST['psw'];
-	 echo $nombre.$yes;
-
-
+    $correo = $_POST['correo'];
+	$contrasena = $_POST['psw'];
+	$Ingreso = false;	
+	
+	$conexion = mysqli_connect("localhost","root","","tynod") or die("Problemas con la conexión");
+	$consulta = mysqli_query($conexion,"select * from usuarios") or die("Problemas en el select:".mysqli_error($conexion));
+	
+	while ($registro=mysqli_fetch_array($consulta))
+	{
+		if ($registro['Correo'] == $correo)
+		{
+			if ($registro['Password'] == $contrasena)
+			{
+				$_SESSION['ID'] = $registro['ID'];
+				$_SESSION['Nombre'] = $registro['Nombre'];
+				$_SESSION['Correo'] = $registro['Correo'];	
+				
+				$Ingreso = true;
+				
+			}
+		}
+	}
+	
+	if ($Ingreso)
+	{
+		echo "Ingresado correctamente";
+	}
+	else
+	{
+		echo "Rectifique sus datos";
+	}
 ?>
